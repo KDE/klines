77f9f3a092afb0e935d8f49ccf0481ac47c9af92
Benjamin Meyer <ben@meyerhome.net>
2004-10-25 01:30:50 +0000

    use qApp to quit
    svn path=/trunk/kdegames/klines/; revision=357457

--- ballpainter.cpp
+++ ballpainter.cpp
@@ -69,7 +69,7 @@ void BallPainter::createPix()
 		locate( "appdata", "fire.jpg" ));
   if (balls->isNull() ||backgroundPix->isNull() || fire->isNull() ) {
       KMessageBox::error(0, i18n("Unable to find graphics. Check your installation."), i18n("Error"));
-      exit(1);
+      qApp->exit(1);
       return; // Error
   }
 
