7ca51d58452c38d391f6a381a85c9d9530c2aa82
Stefan Majewsky <majewsky@gmx.net>
2011-05-30 20:45:03 +0000

    Remove page selection on some KPageDialogs with only one page.
    It's clearly superfluous there.
    
    svn path=/trunk/KDE/kdegames/klines/; revision=1234263

--- klines.cpp
+++ klines.cpp
@@ -179,6 +179,7 @@ void KLinesMainWindow::configureSettings()
         return;
     KConfigDialog *dialog = new KConfigDialog( this, QLatin1String( "settings" ), Prefs::self() );
     dialog->addPage( new KGameThemeSelector( dialog, Prefs::self(), KGameThemeSelector::NewStuffDisableDownload  ), i18n( "Theme" ), QLatin1String( "games-config-theme" ));
+	dialog->setFaceType(KConfigDialog::Plain); //only one page -> no page selection necessary
     connect( dialog, SIGNAL( settingsChanged(const QString&) ), this, SLOT( loadSettings() ) );
     dialog->setHelp(QString(),QLatin1String( "klines" ));
     dialog->show();
